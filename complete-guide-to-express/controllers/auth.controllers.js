b92add1cab5dff2922bcbd31fce16950a2ff3aec
manish0502 <manishgoswami495@gmail.com>
2021-11-02 22:45:27 +0530

    express-basic-2

--- complete-guide-to-express/controllers/auth.controllers.js
+++ complete-guide-to-express/controllers/auth.controllers.js
@@ -1,12 +1,18 @@
 const User = require('../models/user.model')
 
 
+
+/** visit to http://localhost:5000/api/v1/auth/register and you will see the register option */
+
+
 const register =(req, res, next) => {
     res.json({
         msg:"This is from controllers",
         status:200
     })
 }
+
+/** visit to http://localhost:5000/api/v1/auth/login and you will see the login option */
 
 
 const login =(req, res, next) => {
@@ -16,6 +22,9 @@ const login =(req, res, next) => {
     })
 }
 
+/** visit to http://localhost:5000/api/v1/auth/simple and you will see the response */
+
+
 const simple =(req, res, next) => {
     res.json({
         msg:"This is get request from controllers",
@@ -23,10 +32,69 @@ const simple =(req, res, next) => {
     })
 }
 
+/** visit to http://localhost:5000/api/v1/auth/ and you will see the link for the  register */
+
+
+const redirectRegister = (req, res, next) => {
+
+           res.send('<h1>Home</h1><p>Please <a href="/api/v1/auth/register">register</a></p>');
+          };
+
+
+/** visit to http://localhost:5000/api/v1/auth/register and you will see the register page */
+
+
+const registerForm = (req, res, next) => {
+
+    const form = '<h1>Register Page</h1><form method="post" action="register">\
+                    Enter Username:<br><input type="text" name="username">\
+                    <br>Enter Password:<br><input type="password" name="password">\
+                    <br><br><input type="submit" value="Submit"></form>';
+
+    res.send(form);
+    
+}
+
+/** visit to http://localhost:5000/api/v1/auth/login and you will see the login page */
+
+
+const loginForm = (req, res, next) => {
+   
+    const form = '<h1>Login Page</h1><form method="POST" action="/login">\
+    Enter Username:<br><input type="text" name="username">\
+    <br>Enter Password:<br><input type="password" name="password">\
+    <br><br><input type="submit" value="Submit"></form>';
+
+    res.send(form);
+
+}
+
+
+/** visit to http://localhost:5000/api/v1/auth/login-success and you will see the following request */
+
+
+const loginSuccess = (req, res, next) => {
+    res.send('<p>You successfully logged in. --> <a href="/protected-route">Go to protected route</a></p>');
+
+}
+
+
+
+/** visit to http://localhost:5000/api/v1/auth/login-failure and you will see the following request */
+
+
+const loginFailure = (req, res, next) => {
+    res.send('You entered the wrong password.');
+}
 
 
 module.exports = {
     register,
     login,
-    simple
+    simple,
+    redirectRegister,
+    registerForm,
+    loginForm,
+    loginSuccess,
+    loginFailure
   }
--- complete-guide-to-express/routes/auth.router.js
+++ complete-guide-to-express/routes/auth.router.js
@@ -4,19 +4,36 @@ const router = express.Router()
 const {
     register,
     login ,
-    simple
+    simple,
+    redirectRegister,
+    registerForm,
+    loginForm,
+    loginSuccess,
+    loginFailure
      } = require('../controllers/auth.controllers');
 
 
+
+router.route('/')
+     .get(redirectRegister)
+
 router.route('/register')
             .post(register)
+            .get(registerForm)
+
 
 router.route('/login')
             .post(login)
+            .get(loginForm)
 
 router.route('/simple')
             .get(simple)
 
+router.route('/login-success')
+            .get(loginSuccess)
+
+router.route('/login-failure')
+            .get(loginFailure)
 
 
 module.exports = router
