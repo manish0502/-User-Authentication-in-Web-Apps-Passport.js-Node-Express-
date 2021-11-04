06f08958d302c2a41dd3f087b83fea15bb680e3b
manish0502 <manishgoswami495@gmail.com>
2021-11-04 20:33:50 +0530

    auth using PKA

---
+++ public-key-cryptography/controllers/auth.controller.js
@@ -0,0 +1,41 @@
+const User = require('../models/User.model')
+const { StatusCodes } = require('http-status-codes')
+const { BadRequestError, UnauthenticatedError } = require('../errors')
+const asyncWrapper = require("../middleware/async");
+
+const register = asyncWrapper( async (req, res) => {
+  const user = await User.create(req.body)
+  const token = user.createJWT()
+
+  res.status(StatusCodes.CREATED).json({
+    msg: "User has been successfully created",
+    status: StatusCodes.CREATED,
+    user: { name: user.name },
+    token,
+  });
+ 
+})
+
+const login = asyncWrapper(async (req, res) => {
+  const { email, password } = req.body
+
+  if (!email || !password) {
+    throw new BadRequestError('Please provide email and password')
+  }
+  const user = await User.findOne({ email })
+  if (!user) {
+    throw new UnauthenticatedError('Invalid Credentials')
+  }
+  const isPasswordCorrect = await user.comparePassword(password)
+  if (!isPasswordCorrect) {
+    throw new UnauthenticatedError('Invalid Credentials')
+  }
+  // compare password
+  const token = user.createJWT()
+  res.status(StatusCodes.OK).json({ user: { name: user.name }, token })
+})
+
+module.exports = {
+  register,
+  login,
+}
