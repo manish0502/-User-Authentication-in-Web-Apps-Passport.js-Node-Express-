1d3f25096418f1cbc9a97e0de772050aceff84f9
manish0502 <manishgoswami495@gmail.com>
2021-11-04 11:12:09 +0530

    jwt-auth-1

---
+++ authentication-using-JWT/app.js
@@ -0,0 +1,47 @@
+require('dotenv').config();
+var logger = require('morgan')
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const connectDB = require('./config/db');
+
+
+//const authenticateUser = require('./middleware/authentication');
+// routers
+//const authRouter = require('./routes/auth');
+//const jobsRouter = require('./routes/jobs');
+// error handler
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+
+
+
+connectDB()
+
+
+
+
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(cors());
+
+
+app.get('/', (req, res) => {
+  res.send('Hello from App.js');
+});
+
+
+// routes
+// app.use('/api/v1/auth', authRouter);
+// app.use('/api/v1/jobs', authenticateUser, jobsRouter);
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 3000;
+
+
+app.listen(port, ()=>{
+    console.log(`Server is running at port ${port}`);
+});
